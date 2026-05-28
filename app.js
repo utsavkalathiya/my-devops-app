afeff49c14ee8a9a704e6502a2db1a6d1a4337e5
utsavXkalathiya <utsavkalathiya811@gmail.com>
2026-05-28 16:28:21 -0400

    Update pipeline to authenticate and push Docker image to AWS ECR

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
     <h1>Welcome to DevOps!</h1>
     <p>This app is running in a Docker container.</p>
     <p>Version: 1.0</p>
   `);
 });
-
 // Export ONLY the app setup, don't listen here
 module.exports = app;
