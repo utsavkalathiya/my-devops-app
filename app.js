2cb6b365200bfde22a82071af6785415c5dd87b2
utsavXkalathiya <utsavkalathiya811@gmail.com>
2026-05-30 15:35:51 -0400

    Files updated

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-/*const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send(`
-    <h1>Welcome to DevOps!</h1>
-    <p>Welcome to DevOps! My Fully Automated Pipeline is Live!</p>
-    <p>Version: 1.0</p>
-  `);
-});
-// Export ONLY the app setup, don't listen here
-module.exports = app;
-
-*/
-
 const express = require("express");
 const path = require("path");
 const app = express();
